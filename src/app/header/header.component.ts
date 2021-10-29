fc5a4dde314a89c38b2b2f498468ca41ef4f1cf1
Tester Siladi <testersiladi@gmail.com>
2021-10-29 20:24:38 +0200

    Navbar using bootstrap 5, favicons and google fonts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  navbarOpen: Boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
 }
